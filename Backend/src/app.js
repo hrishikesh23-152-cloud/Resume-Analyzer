6b5a64f95fed28c8951351f7aecc87b6b680cd70
Hrishikesh <hrishikesh23-152@aec.ac.in>
2026-04-18 17:57:01 +0530

    Add CORS support for production URL

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,7 +5,10 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173",
+             "https://resume-analyzer-kqqa.onrender.com"
+             ],
+   
     credentials: true
 }))
 
