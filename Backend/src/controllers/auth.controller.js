35a9acd22ab86fa2887297168f5b3f00d02b5f73
Hrishikesh <hrishikesh23-152@aec.ac.in>
2026-04-20 12:17:44 +0530

    Enhance cookie security for JWT token
    Updated cookie settings for token to enhance security.

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -38,7 +38,12 @@ async function registerUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "None",
+  maxAge: 24 * 60 * 60 * 1000
+})
 
 
     res.status(201).json({
@@ -78,7 +83,12 @@ async function loginUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "None",
+  maxAge: 24 * 60 * 60 * 1000
+})
     res.status(200).json({
         message: "User loggedIn successfully.",
         user: {
